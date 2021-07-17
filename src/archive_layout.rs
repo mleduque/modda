14bde1727975d2b2cf3a7a532fc4033be55ace63
Mickaël <mleduque@gmail.com>
2021-07-17 22:50:01 +0200

    add support for multiple dirs archive
    for example EET

--- src/archive_layout.rs
+++ src/archive_layout.rs
@@ -2,49 +2,62 @@
 use serde::{Deserialize, Serialize};
 
 
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Default)]
+pub struct Layout {
+    pub strip_leading: Option<usize>,
+    #[serde(default, flatten)]
+    pub content: LayoutContent,
+}
+
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
 #[serde(tag = "type")]
-pub enum Layout {
+pub enum LayoutContent {
     #[serde(rename = "single_dir")]
-    SingleDir { strip_leading: Option<usize> },
+    SingleDir,
     #[serde(rename = "single_dir_plus_tp2")]
-    SingleDirPlusTp2 { tp2: String, strip_leading: Option<usize> },
+    SingleDirPlusTp2 { tp2: Option<String> },
+    #[serde(rename = "multi_dir")]
+    MultipleDirs { dirs: Vec<String> },
     // other layouts to be added as needed
     // All ?
     // Explicit ?
 }
 
-impl Default for Layout {
+impl Default for LayoutContent {
     fn default() -> Self {
-        Layout::SingleDir { strip_leading: None }
+        LayoutContent::SingleDir
     }
 }
 
 impl Layout {
     pub fn to_glob(&self, module_name: &str) -> Vec<String> {
-        use Layout::*;
+        use LayoutContent::*;
         let prefix = self.strip_pattern();
         let prefix = if prefix.is_empty() {
             prefix
         } else {
             format!("{}/", prefix)
         };
-        match self {
-            SingleDir { .. } => vec![format!("{}{}", prefix, module_name)],
-            SingleDirPlusTp2 { tp2, .. } => vec![
-                    format!("{}{}", prefix, module_name), 
+        match &self.content {
+            SingleDir => vec![format!("{}{}", prefix, module_name)],
+            SingleDirPlusTp2 { tp2: Some(tp2) } => vec![
+                    format!("{}{}", prefix, module_name),
                     format!("{}{}", prefix, tp2),
                 ],
+                SingleDirPlusTp2 { tp2: None } => vec![
+                        format!("{}{}", prefix, module_name),
+                        format!("{}setup-{}.tp2", prefix, module_name),
+                    ],
+            MultipleDirs { dirs } => dirs.iter().map(|dir|
+                    format!("{}{}", prefix, dir)
+                ).collect::<Vec<_>>(),
         }
     }
 
     fn strip_level(&self) -> usize {
-        use Layout::*;
         match self {
-            SingleDir { strip_leading: None } 
-            | SingleDirPlusTp2 { strip_leading: None, .. } => 0,
-            SingleDir { strip_leading: Some(v) } => *v,
-            SingleDirPlusTp2 { strip_leading: Some(v), .. } => *v,
+            Layout { strip_leading: None, .. } => 0,
+            Layout { strip_leading: Some(v), .. } => *v,
         }
     }
 
@@ -55,16 +68,60 @@ impl Layout {
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct FilePattern {
-    pub pattern: String, 
+    pub pattern: String,
     pub subdir: String,
 }
 
 #[cfg(test)]
+impl LayoutContent {
+    pub fn with_tp2_default() -> Self { LayoutContent::SingleDirPlusTp2 { tp2: None } }
+    pub fn with_tp2(tp2: String) -> Self { LayoutContent::SingleDirPlusTp2 { tp2: Some(tp2) } }
+    pub fn multi_dir(dirs: Vec<String>) -> Self { LayoutContent::MultipleDirs { dirs } }
+}
+
+#[cfg(test)]
 impl Layout {
-    pub fn single_dir(level: usize) -> Self { Layout::SingleDir { strip_leading: Some(level) } }
-    pub fn with_tp2(tp2: String) -> Self { Layout::SingleDirPlusTp2 { tp2, strip_leading: None } }
-    pub fn with_tp2_and_strip(tp2: String, level: usize) -> Self { 
-        Layout::SingleDirPlusTp2 { tp2, strip_leading: Some(level) } 
+    pub fn single_dir(strip_lvl: usize) -> Self {
+        Layout {
+            content: LayoutContent::SingleDir,
+            strip_leading: Some(strip_lvl),
+        }
+    }
+    pub fn with_tp2(tp2: String) -> Self {
+        Layout {
+            content: LayoutContent::with_tp2(tp2),
+            ..Self::default()
+        }
+    }
+    pub fn with_tp2_default() -> Self {
+        Layout {
+            content: LayoutContent::with_tp2_default(),
+            ..Self::default()
+        }
+    }
+    pub fn with_tp2_and_strip(tp2: String, strip_lvl: usize) -> Self {
+        Layout {
+            content: LayoutContent::with_tp2(tp2),
+            strip_leading: Some(strip_lvl),
+        }
+    }
+    pub fn with_tp2_default_and_strip(strip_lvl: usize) -> Self {
+        Layout {
+            content: LayoutContent::with_tp2_default(),
+            strip_leading: Some(strip_lvl),
+        }
+    }
+    pub fn multi_dir(dirs: Vec<String>) -> Self {
+        Layout {
+            content: LayoutContent::multi_dir(dirs),
+            ..Self::default()
+        }
+    }
+    pub fn multi_dir_and_strip(dirs: Vec<String>, strip_lvl: usize) -> Self {
+        Layout {
+            content: LayoutContent::multi_dir(dirs),
+            strip_leading: Some(strip_lvl),
+        }
     }
 }
 
@@ -81,6 +138,13 @@ fn test_strip_pattern() {
     assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 1).strip_pattern(), "*");
     assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 2).strip_pattern(), "*/*");
     assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 3).strip_pattern(), "*/*/*");
+
+    let dirs = vec!["a".to_owned(), "b".to_owned()];
+    assert_eq!(Layout::multi_dir(dirs.clone()).strip_pattern(), "");
+    assert_eq!(Layout::multi_dir_and_strip(dirs.clone(), 0).strip_pattern(), "");
+    assert_eq!(Layout::multi_dir_and_strip(dirs.clone(), 1).strip_pattern(), "*");
+    assert_eq!(Layout::multi_dir_and_strip(dirs.clone(), 2).strip_pattern(), "*/*");
+    assert_eq!(Layout::multi_dir_and_strip(dirs.clone(), 3).strip_pattern(), "*/*/*");
 }
 
 #[test]
@@ -91,14 +155,98 @@ fn test_to_glob() {
     assert_eq!(Layout::single_dir(2).to_glob("toto"), vec!["*/*/toto".to_string()]);
     assert_eq!(Layout::single_dir(3).to_glob("toto"), vec!["*/*/*/toto".to_string()]);
 
-    assert_eq!(Layout::with_tp2("a".to_owned()).to_glob("toto"), 
+    assert_eq!(Layout::with_tp2("a".to_owned()).to_glob("toto"),
                                     vec!["toto".to_string(), "a".to_string()]);
-    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 0).to_glob("toto"), 
+    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 0).to_glob("toto"),
                                     vec!["toto".to_string(), "a".to_string()]);
-    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 1).to_glob("toto"), 
+    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 1).to_glob("toto"),
                                     vec!["*/toto".to_string(), "*/a".to_string()]);
-    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 2).to_glob("toto"), 
+    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 2).to_glob("toto"),
                                     vec!["*/*/toto".to_string(), "*/*/a".to_string()]);
-    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 3).to_glob("toto"), 
+    assert_eq!(Layout::with_tp2_and_strip("a".to_owned(), 3).to_glob("toto"),
                                     vec!["*/*/*/toto".to_string(), "*/*/*/a".to_string()]);
+
+
+    assert_eq!(
+        Layout::with_tp2_default().to_glob("toto"),
+        vec!["toto".to_string(), "setup-toto.tp2".to_string()]
+    );
+    assert_eq!(
+        Layout::with_tp2_default_and_strip(1).to_glob("toto"),
+        vec!["*/toto".to_string(), "*/setup-toto.tp2".to_string()]
+    );
+
+    let dirs = vec!["a".to_owned(), "b".to_owned()];
+    assert_eq!(
+        Layout::multi_dir(dirs.clone()).to_glob("toto"),
+        vec!["a".to_string(), "b".to_string()]
+    );
+    assert_eq!(
+        Layout::multi_dir_and_strip(dirs.clone(), 0).to_glob("toto"),
+        vec!["a".to_string(), "b".to_string()]
+    );
+    assert_eq!(
+        Layout::multi_dir_and_strip(dirs.clone(), 1).to_glob("toto"),
+        vec!["*/a".to_string(), "*/b".to_string()]
+    );
+    assert_eq!(
+        Layout::multi_dir_and_strip(dirs.clone(), 2).to_glob("toto"),
+        vec!["*/*/a".to_string(), "*/*/b".to_string()]
+    );
+    assert_eq!(
+        Layout::multi_dir_and_strip(dirs.clone(), 3).to_glob("toto"),
+        vec!["*/*/*/a".to_string(), "*/*/*/b".to_string()]
+    );
+}
+
+#[test]
+fn deserialize_layout_single_dir_missing_strip() {
+    let yaml = r#"
+    type: single_dir
+    "#;
+    let layout: Layout = serde_yaml::from_str(yaml).unwrap();
+    assert_eq!(
+        layout,
+        Layout::default()
+    );
+}
+
+#[test]
+fn deserialize_layout_single_dir_with_strip() {
+    let yaml = r#"
+    strip_leading: 1
+    type: single_dir
+    "#;
+    let layout: Layout = serde_yaml::from_str(yaml).unwrap();
+    assert_eq!(
+        layout,
+        Layout::single_dir(1)
+    );
+}
+
+#[test]
+fn deserialize_layout_single_dir_with_tp2_default() {
+    let yaml = r#"
+    strip_leading: 1
+    type: single_dir_plus_tp2
+    "#;
+    let layout: Layout = serde_yaml::from_str(yaml).unwrap();
+    assert_eq!(
+        layout,
+        Layout::with_tp2_default_and_strip(1)
+    );
+}
+
+#[test]
+fn deserialize_layout_single_dir_with_tp2() {
+    let yaml = r#"
+    strip_leading: 1
+    type: single_dir_plus_tp2
+    tp2: toto.tp2
+    "#;
+    let layout: Layout = serde_yaml::from_str(yaml).unwrap();
+    assert_eq!(
+        layout,
+        Layout::with_tp2_and_strip("toto.tp2".to_string(), 1)
+    );
 }
