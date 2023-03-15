5500f3fc9f2dc378ee9ed619ea1bc039277b5d8a
Mickaël <mleduque@gmail.com>
2023-03-15 10:50:50 +0100

    Move weidu command from language.rs

--- src/language.rs
+++ src/language.rs
@@ -1,21 +1,14 @@
 
-use std::process::Command;
-
 use anyhow::bail;
 use anyhow::Result;
-use lazy_static::lazy_static;
-use regex::Regex;
 
 use crate::module::weidu_mod::WeiduMod;
-
-lazy_static! {
-    static ref LANGUAGE_REGEX: Regex = Regex::new("^([0-9]*):(.*)$").unwrap();
-}
+use crate::run_weidu::list_available_languages;
 
 #[derive(Clone, Debug)]
 pub struct LanguageOption {
-    index: u32,
-    name: String,
+    pub index: u32,
+    pub name: String,
 }
 
 #[derive(Clone, Debug)]
@@ -74,46 +67,4 @@ pub fn select_language(tp2:&str, module: &WeiduMod, lang_preferences: &Option<Ve
             }
         }
     }
-}
-
-fn list_available_languages(tp2: &str, module: &WeiduMod) -> Result<Vec<LanguageOption>> {
-    let mut command = Command::new("weidu");
-    let args = vec![
-        "--no-exit-pause".to_owned(),
-        "--list-languages".to_owned(),
-        tp2.to_owned(),
-    ];
-    command.args(&args);
-    let output = command.output()?;
-
-    // the first line show a version string starting with [weidu], then some lines
-    // with [<some file name>] ...
-    // then n language lines in the form
-    // <integer>COLON<string(language name)>
-    let lines = crate::bufread_raw::BufReadRaw::new(&output.stdout[..]).raw_lines();
-
-    let mut lines_ok = vec![];
-    for line in lines {
-        match line {
-            Err(err) => bail!("Couldn't obtain language list for module '{}' [error reading output] _ {:?}",
-                            module.name, err),
-            Ok(line) => {
-                lines_ok.push(line);
-            }
-        }
-    }
-    let lines_str = lines_ok.iter().map(|line| String::from_utf8_lossy(line)).collect::<Vec<_>>();
-    let entries = lines_str.iter().filter_map(|line| match LANGUAGE_REGEX.captures(line) {
-        None => None,
-        Some(cap) => {
-            match (cap.get(1), cap.get(2)) {
-                (Some(index), Some(name)) => match u32::from_str_radix(index.as_str(), 10) {
-                    Ok(index) => Some((index, name.as_str().to_owned())),
-                    Err(_err) => None
-                }
-                _  => None
-            }
-        }
-    }).collect::<Vec<_>>();
-    Ok(entries.into_iter().map(|(index, name)| LanguageOption { index, name }).collect())
 }
--- src/run_weidu.rs
+++ src/run_weidu.rs
@@ -4,7 +4,9 @@ use std::io::{BufRead};
 use std::process::{Command, Stdio};
 
 use anyhow::{bail, Result};
+use lazy_static::lazy_static;
 use log::error;
+use regex::Regex;
 
 use crate::args::Install;
 use crate::global::Global;
@@ -147,4 +149,51 @@ pub fn run_weidu_list_components(tp2: &str, lang_id: u32) -> Result<Vec<WeiduCom
         return Ok(result);
     }
     bail!("weidu json output not parseable")
+}
+
+
+lazy_static! {
+    static ref LANGUAGE_REGEX: Regex = Regex::new("^([0-9]*):(.*)$").unwrap();
+}
+
+pub fn list_available_languages(tp2: &str, module: &WeiduMod) -> Result<Vec<LanguageOption>> {
+    let mut command = Command::new("weidu");
+    let args = vec![
+        "--no-exit-pause".to_owned(),
+        "--list-languages".to_owned(),
+        tp2.to_owned(),
+    ];
+    command.args(&args);
+    let output = command.output()?;
+
+    // the first line show a version string starting with [weidu], then some lines
+    // with [<some file name>] ...
+    // then n language lines in the form
+    // <integer>COLON<string(language name)>
+    let lines = crate::bufread_raw::BufReadRaw::new(&output.stdout[..]).raw_lines();
+
+    let mut lines_ok = vec![];
+    for line in lines {
+        match line {
+            Err(err) => bail!("Couldn't obtain language list for module '{}' [error reading output] _ {:?}",
+                            module.name, err),
+            Ok(line) => {
+                lines_ok.push(line);
+            }
+        }
+    }
+    let lines_str = lines_ok.iter().map(|line| String::from_utf8_lossy(line)).collect::<Vec<_>>();
+    let entries = lines_str.iter().filter_map(|line| match LANGUAGE_REGEX.captures(line) {
+        None => None,
+        Some(cap) => {
+            match (cap.get(1), cap.get(2)) {
+                (Some(index), Some(name)) => match u32::from_str_radix(index.as_str(), 10) {
+                    Ok(index) => Some((index, name.as_str().to_owned())),
+                    Err(_err) => None
+                }
+                _  => None
+            }
+        }
+    }).collect::<Vec<_>>();
+    Ok(entries.into_iter().map(|(index, name)| LanguageOption { index, name }).collect())
 }
