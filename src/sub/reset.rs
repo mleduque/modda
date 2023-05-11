7579395b40cca80ff266cee8ae1705d9eac32660
Mickaël <mleduque@gmail.com>
2023-05-11 14:07:52 +0200

    💬 Add newline in reset prompy

--- src/sub/reset.rs
+++ src/sub/reset.rs
@@ -49,7 +49,7 @@ pub fn reset(args: &Reset, game_dir: &CanonPath, config: &Config) -> Result<()> 
         _=> bail!("Found multiple occurrences of mod/component in weidu.log - aborting reset"),
     };
     let removed = &installed[(index - 1)..];
-    let prompt = format!("Will uninstall these (in reverse order)\n  {}", removed.iter().map(|item| item.short()).join("\n  "));
+    let prompt = format!("Will uninstall these (in reverse order)\n  {}\nProceed? ", removed.iter().map(|item| item.short()).join("\n  "));
     if dialoguer::Confirm::new().with_prompt(prompt).interact()? {
         for fragment in removed.iter().rev() {
             let tp2 = find_tp2_str(game_dir, &weidu_mod.name)?;
